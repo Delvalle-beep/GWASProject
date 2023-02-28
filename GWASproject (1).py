7f7cf213b62b87bcf2dfd67fb066eea7fff009a5
Vitoria Regina Oliveira <vitoria_regina.oliveira@roche.com>
2023-02-28 19:00:45 +0100

    Updating

--- GWASproject (1).py
+++ GWASproject (1).py
@@ -17,7 +17,8 @@ def process_paths(input_path,output_path):
     print('Processing the paths...')
     return input_path, output_path
 
-def process_data(input_path,output_path,cut,region_mqq,marker_size,sig_level,skip,highlight,highlight_color,pinpoint,pinpoint_color, anno):
+def process_data(input_path,output_path,cut,region_mqq,marker_size,sig_level,skip,highlight,highlight_color,pinpoint,pinpoint_color, anno,
+                region_regPlot,vcf_path,vcf_chr_dict):
     i = 0
     for input_path in input_path:
         df=pd.read_csv(input_path,sep="\t",nrows=100000)
@@ -65,7 +66,6 @@ def process_data(input_path,output_path,cut,region_mqq,marker_size,sig_level,ski
         file_name2 = output_path + 'Regional' + str(i) +'.pdf'
         file_name3 = output_path + 'Regional_other' + str(i) +'.pdf'
         
-        #Manhattan and QQ plot
         mysumstats.plot_mqq(save=file_name,
                         saveargs={"dpi":400,"facecolor":"white"},
                         marker_size=marker_size,
@@ -81,10 +81,33 @@ def process_data(input_path,output_path,cut,region_mqq,marker_size,sig_level,ski
                       )
         
         mysumstats.get_lead()
-
-        #Regional Plot 
-        mysumstats.plot_mqq(save= file_name2,region=(7,156538803,157538803))
-        mysumstats.plot_mqq(save= file_name3, mode="r", region=(7,156538803,157538803),region_grid=True, gtf_path="ensembl")
+        mysumstats.plot_mqq(save= file_name2,
+                           region=region_regPlot,
+                           anno=anno,
+                           cut=cut,
+                           skip=skip,
+                           highlight=highlight,
+                           highlight_color=highlight_color,
+                           pinpoint=pinpoint,
+                           pinpoint_color=pinpoint_color,
+                           sig_level=sig_level,
+                           vcf_path=vcf_path,
+                           vcf_chr_dict=get_number_to_chr(vcf_chr_dict))
+        mysumstats.plot_mqq(save= file_name3,
+                            mode="r",
+                            region=region_regPlot,
+                            region_grid=True,
+                            anno=anno,
+                            cut=cut,
+                            skip=skip,
+                            gtf_path="ensembl",
+                            highlight=highlight,
+                            highlight_color=highlight_color,
+                            pinpoint=pinpoint,
+                            pinpoint_color=pinpoint_color,
+                            sig_level=sig_level,
+                            vcf_path=vcf_path,
+                            vcf_chr_dict=get_number_to_chr(vcf_chr_dict))
         
         i +=1
 
@@ -95,15 +118,18 @@ parser = argparse.ArgumentParser(description='Process multiple input paths')
 parser.add_argument('--input_path', type=str, nargs='+', help='input paths')
 parser.add_argument('--output_path', type=str, help='output path')
 parser.add_argument('--sig_level', type=float,default=5e-8,help='sig_level value for plot_mqq')
-parser.add_argument('--cut', type=int, default=None, help='cut value for plot_mqq')
+parser.add_argument('--cut', type=int, default=0, help='cut value for plot_mqq')
 parser.add_argument('--region_mqq', type=tuple, default=None, help='region value for plot_mqq')
+parser.add_argument('--region_regPlot',type=tuple, default=None, help='region value for regional plot')
 parser.add_argument('--marker_size', type=int, default=(5,25), help='marker_size value for plot_mqq')
 parser.add_argument('--skip', type=int, default=0, help='skip value for plot_mqq')
 parser.add_argument('--highlight', type=str, default=[], help='highlight value for plot_mqq')
 parser.add_argument('--highlight_color', type=str, default='#CB132D', help='highlight_color value for plot_mqq')
 parser.add_argument('--pinpoint', type=str, default=[], help='pinpoint value for plot_mqq')
 parser.add_argument('--pinpoint_color', type=str, default="red", help='pinpoint_color value for plot_mqq')
-parser.add_argument('--anno', type=bool, default=True, help='The variants to annotate will be selected automatically using a sliding window with windowsize=500kb')
+parser.add_argument('--anno',type=bool,default=True,help='The variants to annotate will be selected automatically using a sliding window with windowsize=500kb')
+parser.add_argument('--vcf_path',type=str,default=None,help='To load a reference genotype')
+parser.add_argument('--vcf_chr_dict',type=str,default=None,help='Select a specific region of the reference genotype')
 
 #parsing the arguments
 args = parser.parse_args()
@@ -112,8 +138,7 @@ args = parser.parse_args()
 input_path, output_path = process_paths(args.input_path, args.output_path)
 
 #processing the data
-process_data(args.input_path, args.output_path, args.cut, args.region_mqq, args.marker_size, args.sig_level, args.skip,args.highlight,args.highlight_color, args.pinpoint, args.pinpoint_color, args.anno)
-
+process_data(args.input_path, args.output_path, args.cut, args.region_mqq, args.marker_size, args.sig_level, args.skip,args.highlight,args.highlight_color, args.pinpoint, args.pinpoint_color, args.anno,args.region_regPlot,args.vcf_path,args.vcf_chr_dict)
 
 # In[ ]:
 
--- README.md
+++ README.md
@@ -36,63 +36,63 @@ mysumstats.plot_mqq(
           build="19",
           region_mqq=None,
           saveargs={"dpi":400,"facecolor":"white"}
-          ):
+          )
 ```
 <ul>
 <li><b>Skip:</b> sometimes it is not necessary to plot all variants, we can skip the insignicant variants . For example, we can exclude varints with -log10p lower than 3 from the plot by specifying skip=3</li>
 Use this command to set:
 </br>
 ```
---skip
+--skip "0"
 ```
 </br>
 <li><b>Cut:</b>loci with extremly large -log10(P) value are very likely to dwarf other significant loci , so we want to scale down the extrame loci from a certain threshold.</li>
 Use this command to set:
 </br>
 ```
---cut
+--cut "0"
 ```
 </br>
 <li><b>Sig_level:</b>genome-wide significance threshold. Specify the P value threshold.</li>
 </br>
 Use this command to set:
 ```
---sig_level
+--sig_level "5e-8"
 ```
 </br>
 <li><b>Anno:</b>When it is set "True", the variants to annotate will be selected automatically using a sliding window with windowsize=500kb.</li>
 </br>
 Use this command to set:
 ```
---anno
+--anno True
 ```
 </br>
 <li><b>Highlight:</b>A "type=list" element to specify the variants of loci for highlighting.</li>
 </br>
 Use this command to set:
 ```
---highlight
+--highlight ["rs12509595","19:15040733:T:C"]
 ```
 </br>
 <li><b>Highlight_color:</b>specify the color ussed for highlighting, RGB format.</li>
 </br>
 Use this command to set:
 ```
---highlight_color
+--highlight_color "#CB132D"
 ```
 </br>
 <li><b>Pinpoint:</b>a list of SNPIDs.</li>
 </br>
 Use this command to set:
 ```
---pinpoint
+--pinpoint ["rs7989823"]
 ```
 </br>
 <li><b>Pinpoint_color:</b>color for pinpoint.</li>
 </br>
 Use this command to set:
 ```
---pinpoint_color
+--pinpoint_color "red"
 ```
 </br>
 <li><b>Build:</b>genome build version "19" or "38".</li>
@@ -101,14 +101,59 @@ Use this command to set:
 </br>
 Use this command to set:
 ```
---region_mqq
+--region_mqq (7,,156538803,157538803) 
 ```
 </br>
 <li><b>marker_sizer:</b>It define the marker size,is defined by two integers, an example of use is:marker_size=(5,10)</li>
 </br>
 Use this command to set:
 ```
---marker_size
+--marker_size (5,25)
+```
+</br>
+</ul>
+
+#Regional Plot
+
+For the Regional Plot  the default values ​​are the following:
+```
+mysumstats.plot_mqq(
+          anno=True,
+          cut=0,
+          skip=0,
+          sig_level=5e-8,
+          highlight = [],
+          highlight_color="#CB132D",
+          pinpoint=[],
+          pinpoint_color ="red",
+          region_regPlot=None,
+          vcf_path=None,
+          vcf_chr_dict=None,
+          mode="r"
+          )
+```
+Many of the arguments are also used in the Manhattan and QQ plots and have the same definition, the definition of additional arguments will be added below.
+
+<ul>
+<li><b>Region_regPlot:</b> sometimes it is not necessary to plot all variants, we can skip the insignicant variants . For example, we can exclude varints with -log10p lower than 3 from the plot by specifying skip=3. This argument is used to uniquely define the region plotted on regional plots only.</li>
+Use this command to set:
+</br>
+```
+--region_regPlot (7,,156538803,157538803)
+```
+</br>
+<li><b>Vcf_path:</b>If you want to load a reference genotype just add the path</li>
+Use this command to set:
+</br>
+```
+--vcf_path 'C:/This/Is/My/Path'
+```
+</br>
+<li><b>Vcf_chr_dict:</b>This argument is defined to select one or a portion of chromosomes to be used in the reference genotype. This already has a built-in get() function that filters the chromosome you would like to reference.</li>
+</br>
+Use this command to set:
+```
+--vcf_chr_dict '6'
 ```
 </br>
 </ul>
